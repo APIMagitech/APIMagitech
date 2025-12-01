3dc973772d609fee97e76404d8b706603c101b9a
GABRIEL DUARTE MELO <41049455878@info.local>
2025-12-01 20:25:17 -0300

    Novo modelo API

---
+++ Magitech API/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using MagitechAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configurar porta para SquareCloud
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
+Console.WriteLine("Connection String: " + builder.Configuration.GetConnectionString("DefaultConnection"));
+
+// Adicionar serviços
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configurar banco de dados
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Configurar CORS para permitir seu site React
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReact", policy =>
+    {
+        policy.WithOrigins(
+            "http://localhost:5173",
+            "http://localhost:3000",
+            "https://seu-site.com"
+        )
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// Configurar pipeline HTTP - Swagger sempre ativo
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseCors("AllowReact");
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
